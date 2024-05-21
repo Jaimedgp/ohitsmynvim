8c836fef3941aa582d3447665ffb26f40e4321bc
jaimedgp <jaimedgp@predictia.es>
2024-05-21 09:34:06 +0200

    always show .gitignore and .gitlab-ci.ymal

--- lua/jaimedgp/plugins/neo-tree.lua
+++ lua/jaimedgp/plugins/neo-tree.lua
@@ -197,7 +197,8 @@ return {
 							--"*/src/*/tsconfig.json",
 						},
 						always_show = { -- remains visible even if other settings would normally hide it
-							--".gitignored",
+							".gitignored",
+                            ".gitlab-ci.yml"
 						},
 						never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
 							--".DS_Store",
