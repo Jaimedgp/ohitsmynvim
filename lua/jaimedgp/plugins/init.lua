88ec531a71ede96ef2a3b8e75554196f4a289cad
Jaimedgp <jaimediezgp@gmail.com>
2024-05-27 18:57:43 +0200

    Setup notify

--- lua/jaimedgp/plugins/init.lua
+++ lua/jaimedgp/plugins/init.lua
@@ -6,7 +6,6 @@
 -- d: debugging
 
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+    "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+    "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 }
