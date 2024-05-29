2a106ecdfe0b5b17f88b8fcbc051dca58ee5cf10
jaimedgp <jaimedgp@predictia.es>
2024-05-29 17:37:50 +0200

    Folding configuration

---
+++ lua/jaimedgp/plugins/folding.lua
@@ -0,0 +1,92 @@
+return {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    config = function()
+        vim.o.foldcolumn = '3' -- '0' is not bad
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+        vim.o.fillchars = [[foldopen:,foldsep:⏐,foldclose:]]
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
+        local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+        for _, ls in ipairs(language_servers) do
+            require('lspconfig')[ls].setup({
+                capabilities = capabilities
+                -- you can add other fields for setting up lsp server in this table
+            })
+        end
+
+        local handler = function(virtText, lnum, endLnum, width, truncate)
+            local newVirtText = {}
+            local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+            local sufWidth = vim.fn.strdisplaywidth(suffix)
+            local targetWidth = width - sufWidth
+            local curWidth = 0
+            for _, chunk in ipairs(virtText) do
+                local chunkText = chunk[1]
+                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                if targetWidth > curWidth + chunkWidth then
+                    table.insert(newVirtText, chunk)
+                else
+                    chunkText = truncate(chunkText, targetWidth - curWidth)
+                    local hlGroup = chunk[2]
+                    table.insert(newVirtText, {chunkText, hlGroup})
+                    chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    -- str width returned from truncate() may less than 2nd argument, need padding
+                    if curWidth + chunkWidth < targetWidth then
+                        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                    end
+                    break
+                end
+                curWidth = curWidth + chunkWidth
+            end
+            table.insert(newVirtText, {suffix, 'MoreMsg'})
+            return newVirtText
+        end
+
+        require('ufo').setup({
+            -- provider_selector = function(bufnr, filetype, buftype)
+            --     return { "lsp", "treesitter", "indent" }
+            -- end,
+            open_fold_hl_timeout = 150,
+            fold_virt_text_handler = handler,
+            close_fold_kinds_for_ft = {
+                default = {'imports', 'comment'},
+                json = {'array'},
+                c = {'comment', 'region'}
+            },
+            preview = {
+                win_config = {
+                    border = {'', '─', '', '', '', '─', '', ''},
+                    winhighlight = 'Normal:Folded',
+                    winblend = 0
+                },
+                mappings = {
+                    scrollU = '<C-u>',
+                    scrollD = '<C-d>',
+                    jumpTop = '[',
+                    jumpBot = ']'
+                }
+            }
+        })
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        vim.keymap.set('n', '+', 'zo')
+        vim.keymap.set('n', '-', 'zc')
+        vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+        vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+        vim.keymap.set('n', 'K', function()
+            local winid = require('ufo').peekFoldedLinesUnderCursor()
+            if not winid then
+                -- choose one of coc.nvim and nvim lsp
+                vim.fn.CocActionAsync('definitionHover') -- coc.nvim
+                vim.lsp.buf.hover()
+            end
+        end)
+    end,
+}
