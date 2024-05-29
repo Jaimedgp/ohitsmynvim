4a747e7f596987630ac75ffa1bf9718f03f2043a
jaimedgp <jaimedgp@predictia.es>
2024-05-29 16:34:48 +0200

    Config notification UI

--- lua/jaimedgp/plugins/notify.lua
+++ lua/jaimedgp/plugins/notify.lua
@@ -2,7 +2,10 @@ return {
     "rcarriga/nvim-notify",
     config = function ()
         require("notify").setup({
-            background_colour = "#000000"
+            background_colour = "#000000",
+            max_width = 100,
+            max_height = 250,
+            render = "wrapped-compact",
         })
         vim.notify = require("notify")
     end
