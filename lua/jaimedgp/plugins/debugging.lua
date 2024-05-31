d6bf27bf21997d561ede075a8a6a523016aa9010
jaimedgp <jaimedgp@predictia.es>
2024-05-31 09:31:52 +0200

    Using correct icons

--- lua/jaimedgp/plugins/debugging.lua
+++ lua/jaimedgp/plugins/debugging.lua
@@ -25,19 +25,19 @@ return {
 
             vim.fn.sign_define(
                 "DapBreakpoint",
-                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
             )
             vim.fn.sign_define(
                 "DapBreakpointCondition",
-                { text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+                { text = "󰟃", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
             )
             vim.fn.sign_define(
                 "DapBreakpointRejected",
-                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
             )
             vim.fn.sign_define(
                 "DapLogPoint",
-                { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+                { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
             )
             vim.fn.sign_define(
                 "DapStopped",
