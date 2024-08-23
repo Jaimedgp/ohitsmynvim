8f709bd037c6078b7afa646235f96e94a68872f7
jaimedgp <jaimedgp@predictia.es>
2024-08-23 11:42:27 +0200

    Update folding

--- lua/jaimedgp/plugins/folding.lua
+++ lua/jaimedgp/plugins/folding.lua
@@ -8,19 +8,6 @@ return {
         vim.o.foldenable = true
         vim.o.fillchars = [[foldopen:,foldclose:]] -- ⏐
 
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true
-        }
-        local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-        for _, ls in ipairs(language_servers) do
-            require('lspconfig')[ls].setup({
-                capabilities = capabilities
-                -- you can add other fields for setting up lsp server in this table
-            })
-        end
-
         local handler = function(virtText, lnum, endLnum, width, truncate)
             local newVirtText = {}
             local suffix = (' 󰁂 %d '):format(endLnum - lnum)
@@ -49,9 +36,25 @@ return {
             return newVirtText
         end
 
+        -- Option 2: nvim lsp as LSP client
+        -- Tell the server the capability of foldingRange,
+        -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
+        local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+        for _, ls in ipairs(language_servers) do
+            require('lspconfig')[ls].setup({
+                capabilities = capabilities
+                -- you can add other fields for setting up lsp server in this table
+            })
+        end
+
         require('ufo').setup({
             provider_selector = function(bufnr, filetype, buftype)
-                return { "lsp", "treesitter", "indent" }
+                return { "lsp", }
             end,
             open_fold_hl_timeout = 150,
             fold_virt_text_handler = handler,
