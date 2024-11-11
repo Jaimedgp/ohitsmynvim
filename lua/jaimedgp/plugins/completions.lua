cd4e7498f46ca394986612d05bfb9228c3303563
jaimedgp <jaimedgp@predictia.es>
2024-11-11 09:44:17 +0100

    Update completions

--- lua/jaimedgp/plugins/completions.lua
+++ lua/jaimedgp/plugins/completions.lua
@@ -1,12 +1,13 @@
 return {
-    {
-        "hrsh7th/cmp-nvim-lsp",
-    },
+    { "hrsh7th/cmp-nvim-lsp", },
+    { "rafamadriz/friendly-snippets" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "saadparwaiz1/cmp_luasnip" },
     {
         "L3MON4D3/LuaSnip",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-        },
+        dependencies = { "saadparwaiz1/cmp_luasnip", },
     },
     {
         "hrsh7th/nvim-cmp",
@@ -19,17 +20,17 @@ return {
 
             cmp.setup({
                 snippet = {
-                -- REQUIRED - you must specify a snippet engine
-                expand = function(args)
-                    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                end,
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    end,
                 },
                 window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
--- lua/jaimedgp/plugins/lspconfig.lua
+++ lua/jaimedgp/plugins/lspconfig.lua
@@ -61,51 +61,124 @@ return {
         config = function(_, opts)
             local lspconfig = require("lspconfig")
 
+            lspconfig.ruff_lsp.setup({})
+            lspconfig.markdown_oxide.setup({
+                filetypes = { "markdown" },
+                root_dir = lspconfig.util.root_pattern(".git", "."),
+                settings = {
+                    markdown = {
+                        lint = true,      -- Enable linting
+                        format = true,    -- Enable formatting
+                        validate = true,  -- Enable validation/syntax checking
+                    },
+                },
+            })
+            lspconfig.pyright.setup({})
+            lspconfig.lua_ls.setup({})
+            -- lspconfig.r_language_server.setup({})
+            lspconfig.dockerls.setup({})
+            lspconfig.docker_compose_language_service.setup({})
+            lspconfig.bashls.setup({})
+            lspconfig.html.setup({})
+
             -- Show line diagnostics automatically in hover window
             vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-
-            -- Configure `ruff-lsp`.
-            -- local configs = require 'lspconfig.configs'
-            -- if not configs.ruff_lsp then
-            --     configs.ruff_lsp = {
-            --     }
-            -- end
-            local on_attach = function(client, bufnr)
-                local bufopts = { noremap=true, silent=true, buffer=bufnr }
-            end
-
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            lspconfig.ruff_lsp.setup({
-                on_attach = on_attach,
-                capabilites = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilites = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilites = capabilities,
-            })
-            -- lspconfig.r_language_server.setup({
-            -- capabilites = capabilities,
-            -- })
-            lspconfig.dockerls.setup({
-                capabilites = capabilities,
-            })
-            lspconfig.docker_compose_language_service.setup({
-                capabilites = capabilities,
-            })
-            lspconfig.bashls.setup({
-                capabilites = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilites = capabilities,
-            })
         end,
     },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        dependencies = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" }, -- Required
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "L3MON4D3/LuaSnip" }, -- Required
+        },
+        config = function()
+            local lsp = require("lsp-zero")
+
+            lsp.on_attach(function(client, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+
+                -- vim.keymap.set(
+                --     "n", "gr",
+                --     function() vim.lsp.buf.references() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Reference" })
+                -- )
+                vim.keymap.set(
+                    "n", "gr",
+                    function() require("telescope.builtin").lsp_references() end,
+                    vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Reference" })
+                )
+                vim.keymap.set(
+                    "n", "gD",
+                    function() vim.lsp.buf.definition() end,
+                    vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Definition" })
+                )
+                vim.keymap.set(
+                    "n", "gd",
+                    function()
+                        vim.cmd("vsplit")
+                        vim.lsp.buf.definition()
+                    end,
+                    vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Definition" })
+                )
+                -- vim.keymap.set(
+                --     "n", "K",
+                --     function() vim.lsp.buf.hover() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Hover" })
+                -- )
+                -- vim.keymap.set(
+                --     "n", "<leader>vws",
+                --     function() vim.lsp.buf.workspace_symbol() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Workspace Symbol" })
+                -- )
+                -- vim.keymap.set(
+                --     "n", "<leader>vd",
+                --     function() vim.diagnostic.setloclist() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Show Diagnostics" })
+                -- )
+                vim.keymap.set(
+                    "n", "[d",
+                    function() vim.diagnostic.goto_next() end,
+                    vim.tbl_deep_extend("force", opts, { desc = "Next Diagnostic" })
+                )
+                vim.keymap.set("n"
+                    , "]d",
+                    function() vim.diagnostic.goto_prev() end,
+                    vim.tbl_deep_extend("force", opts, { desc = "Previous Diagnostic" })
+                )
+                -- vim.keymap.set("n"
+                --     , "<leader>vca",
+                --     function() vim.lsp.buf.code_action() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Code Action" })
+                -- )
+                -- vim.keymap.set("n"
+                --     , "<leader>vrr",
+                --     function() vim.lsp.buf.references() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP References" })
+                -- )
+                -- vim.keymap.set(
+                --     "n", "<leader>vrn",
+                --     function() vim.lsp.buf.rename() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Rename" })
+                -- )
+                -- vim.keymap.set(
+                --     "i", "<C-h>",
+                --     function() vim.lsp.buf.signature_help() end,
+                --     vim.tbl_deep_extend("force", opts, { desc = "LSP Signature Help" })
+                -- )
+            end)
+
+            handlers = {
+                lsp.default_setup,
+                lua_ls = function()
+                    local lua_opts = lsp.nvim_lua_ls()
+                    require("lspconfig").lua_ls.setup(lua_opts)
+                end,
+            }
+        end,
+    }
 }
-
-
--- {
---     "neovim/nvim-lspconfig",
---   },
