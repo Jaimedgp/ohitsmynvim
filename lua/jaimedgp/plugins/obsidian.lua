10586ebb5af119111326a51c6bbc9e67f842d734
jaimedgp <jaimedgp@predictia.es>
2024-11-11 09:44:31 +0100

    Update Obsidian

--- lua/jaimedgp/plugins/obsidian.lua
+++ lua/jaimedgp/plugins/obsidian.lua
@@ -1,4 +1,5 @@
 return {
+{
 	"epwalsh/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = false,
@@ -17,9 +18,13 @@ return {
 		-- see below for full list of optional dependencies 👇
 	},
 	keys = {
-		{ "<leader>od", ":ObsidianToday<CR>", "n" },
-		{ "<leader>ot", ":ObsidianTomorrow<CR>", "n" },
-		{ "<leader>oy", ":ObsidianYesterday<CR>", "n" },
+		{ "<leader>o"  , ":ObsidianNew<CR>", "n" },
+		{ "<leader>od" , ":ObsidianToday<CR>", "n" },
+		{ "<leader>ot" , ":ObsidianTomorrow<CR>", "n" },
+		{ "<leader>oy" , ":ObsidianYesterday<CR>", "n" },
+		{ "<leader>of" , ":ObsidianQuickSwitch<CR>", "n" },
+		{ "<leader>otg", ":ObsidianTag<CR>", "n" },
+		{ "<leader>or" , ":ObsidianBackLinks<CR>", "n" },
 	},
 	config = function()
 		require("obsidian").setup({
@@ -38,12 +43,37 @@ return {
 			},
 			-- Optional, for templates (see below).
 			templates = {
-				subdir = "Templates/",
+				subdir = "~/Documents/Obsidian/Predictia/Templates/",
 				date_format = "%Y-%m-%d",
 				time_format = "%H:%M",
 				-- A map for custom variables, the key should be the variable and the value a function
 				substitutions = {},
 			},
+            open_notes_in = "vsplit", -- current | vsplit | hsplit
+            -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+            completion = {
+                -- Set to false to disable completion.
+                nvim_cmp = true,
+                -- Trigger completion at 2 chars.
+                min_chars = 2,
+            },
+            mapping = {
+            -- -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+            -- ["og"] = {
+            --     action = function()
+            --         return require("obsidian").util.gf_passthrough()
+            --     end,
+            --     opts = { noremap = false, expr = true, buffer = true },
+            -- },
+            -- -- Toggle check-boxes.
+            -- ["<leader>ch"] = {
+            --     action = function()
+            --         return require("obsidian").util.toggle_checkbox()
+            --     end,
+            --     opts = { buffer = true },
+            -- },
+            },
 		})
 	end,
+},
 }
